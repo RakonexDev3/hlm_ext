c8d6fcb7c964aa7f1231ba960469b0ea914fff34
Aravind R <aravindrajeshchry@gmail.com>
2026-09-18 13:07:58 +0530

    feat: Add doctype installation team

---
+++ halamama_ext/halamama_extension/doctype/installation_team/installation_team.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Tinu Maria and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Installation Team", {
+// 	refresh(frm) {
+
+// 	},
+// });
