d240f79b8f4eba73d551e63a706741d8a3356b1e
tinumaria <tinumariadavis@gmail.com>
2026-09-21 15:37:49 +0530

    feat: Add doctype delivery zone

---
+++ halamama_ext/halamama_extension/doctype/delivery_zone/test_delivery_zone.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Tinu Maria and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDeliveryZone(FrappeTestCase):
+	pass
